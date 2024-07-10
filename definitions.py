5d01d286e285982f28608c4b71105e436b0ed544
Jesse Geerts <jessegeerts@gmail.com>
2024-07-10 17:33:21 +0100

    add color palette

--- definitions.py
+++ definitions.py
@@ -3,6 +3,7 @@ import os
 ROOT_FOLDER = os.path.dirname(os.path.abspath(__file__))
 WANDB_KEY = '9f4a033fffce45cce1ee2d5f657d43634a1d2889'
 ATTENTION_CMAP = 'inferno'
+COLOR_PALETTE = 'Set1'
 model_save_dir = os.path.join(ROOT_FOLDER, 'models')
 if not os.path.exists(model_save_dir):
     os.makedirs(model_save_dir)
