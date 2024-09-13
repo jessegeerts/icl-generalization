0524e83a829b2a1706dfd97260dbd7e7ce72851d
Jesse Geerts <jessegeerts@gmail.com>
2024-09-13 13:49:49 +0100

    add omniglot embedder module

--- input_embedders.py
+++ input_embedders.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import numpy as np
 from datasets.gauss_datasets import get_mus_label_class
 from models import get_sinusoidal_positional_embeddings_2
+import h5py as h5
 
 
 class GaussianEmbedder(nn.Module):
@@ -142,3 +143,79 @@ class GaussianEmbedderForOrdering(nn.Module):
                                                                   shifts[shift_choice]: shifts[
                                                                                             shift_choice] + seq_len]
         return inputs.to(self.device)
+
+
+class OmniglotEmbedder(nn.Module):
+    """A simple Omniglot embedder for the input space of the transformer model.
+    Note: this inherits from nn.Module, but it doesn't have any trainable parameters.
+    """
+    def __init__(self, config, device):
+        super(OmniglotEmbedder, self).__init__()
+        self.config = config
+        # embedidngs are stored in a file. this class simply reads them
+        embeddings_file = 'datasets/omniglot_resnet18_randomized_order_s0.h5'
+        with h5.File(embeddings_file, 'r') as f:
+            embeddings = torch.Tensor(np.array(f['resnet18/224/feat']))
+        # we only take the first exemplar from each class, for now
+        embeddings = embeddings[:, 0, :]
+        self.pos_embedding_type = config.model.pos_emb_type
+        num_classes, emb_dim = embeddings.shape
+        assert emb_dim == config.data.D
+        self.embeddings = embeddings
+        self.D = config.data.D  # dimension of the embeddings (512 in the omniglot case)
+        self.Nmax = config.data.Nmax
+        self.N = config.seq.N
+        self.L = config.data.L
+        self.label_embeddings = torch.normal(0, 1, size=(self.L, self.D)) / np.sqrt(self.D)
+        self.S = config.train.batch_size
+        self.device = device
+
+    def forward(self, batch):
+        examples = batch['example']
+        labels = batch['label']
+
+        seq_len = examples.shape[1] + labels.shape[1] - 1
+        inputs = torch.zeros((self.config.train.batch_size, seq_len, self.D + 2 * self.Nmax ))
+
+        # fill every first 2 indices with class examples
+        inputs[:, ::3, 2 * self.Nmax:] = self.embeddings[examples[:, ::2]]
+        inputs[:, 1::3, 2 * self.Nmax:] = self.embeddings[examples[:, 1::2]]
+        # fill every 3rd index with label examples (except the last one)
+        inputs[:, 2::3, 2 * self.Nmax:] = self.label_embeddings[labels[:, :-1]]
+
+        # add positional encoding (random shifts)
+        if self.config.model.add_pos_encodings:
+            shifts = np.random.choice((2 * self.Nmax + 2) - (2 * self.N + 2) + 1, size=(self.S))
+            for s in range(self.S):
+                if self.config.model.pos_emb_randomization == 'per_batch':
+                    shift_choice = 0
+                    write_to_example = s
+                elif self.config.model.pos_emb_randomization == 'per_sequence':
+                    shift_choice = s
+                    write_to_example = s
+                elif self.config.model.pos_emb_randomization == 'only_first':
+                    shift_choice = 0
+                    write_to_example = 0
+                elif self.config.model.pos_emb_randomization == 'only_last':
+                    shift_choice = 0
+                    write_to_example = self.S - 1
+                elif self.config.model.pos_emb_randomization == 'no_shift':
+                    shifts = np.zeros(self.S, dtype=int)
+                    shift_choice = 0
+                    write_to_example = s
+                else:
+                    raise ValueError('Invalid positional embedding randomization: {}'.format(
+                        self.config.model.pos_emb_randomization))
+
+                if self.pos_embedding_type == 'onehot':
+                    inputs[write_to_example, :,
+                    shifts[shift_choice]:shifts[shift_choice] + seq_len] = torch.Tensor(
+                        np.identity(seq_len)).to(self.device)
+                else:
+                    inputs[write_to_example, :, :2 * self.Nmax] = self.positional_embedding[0,
+                                                                  shifts[shift_choice]: shifts[
+                                                                                            shift_choice] + seq_len]
+
+        return inputs.to(self.device)
+
+
