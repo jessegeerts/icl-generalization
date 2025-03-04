910fdc16e905d03a43c0be2dd06a1b98c88b636e
Jesse Geerts <jessegeerts@gmail.com>
2025-03-04 11:54:26 +0000

    plot bar of query attention

--- main_utils.py
+++ main_utils.py
@@ -11,6 +11,10 @@ def log_att_weights(i, out_dict, config):
                 axes[h].imshow(out_dict[f'block_{l}']['weights'].mean(axis=0)[h].cpu(), cmap=ATTENTION_CMAP)
             else:
                 axes.imshow(out_dict[f'block_{l}']['weights'].mean(axis=0)[h].cpu(), cmap=ATTENTION_CMAP)
+            if l == 1:
+                # plot bar graph of final layer (query) attention weights for one example for each head
+                plt.figure()
+                plt.bar(range(len(out_dict[f'block_{l}']['weights'][0,-1])), out_dict[f'block_{l}']['weights'][0, -1].cpu())
         if config.log.log_to_wandb:
             wandb.log({f'l{l}_attn_map_icl': fig,
                        'iter': i})  # note: now we're logging the mean of the attention weights across the batch
